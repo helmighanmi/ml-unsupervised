5d8bba92265184905fd924fc6f4fe0f5d37edd21
GHANMI Helmi <40611217+helmighanmi@users.noreply.github.com>
2025-09-16 16:32:02 +0000

    add init

--- src/__init__.py
+++ src/__init__.py
@@ -4,3 +4,9 @@ Date: 2025-09-16
 Position: Data-Science
 """
 
+"""
+ml-unsupervised package
+Unsupervised machine learning tools for clustering, dimensionality reduction, and evaluation.
+"""
+
+__version__ = "0.1.0"
